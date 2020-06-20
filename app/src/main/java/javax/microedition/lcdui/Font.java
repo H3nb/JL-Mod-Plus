Wasim421/SamaraJAR

Use ttf font for virtual keyboard

app/src/main/java/javax/microedition/lcdui/Font.java
@@ -17,6 +17,7 @@
 
 package javax.microedition.lcdui;
 
+import android.content.res.AssetManager;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.util.TypedValue;
@@ -94,7 +95,10 @@ public class Font {
 
 	// Font for keyboard
 	public Font() {
+		AssetManager manager = ContextHolder.getContext().getAssets();
+		Typeface typeface = Typeface.createFromAsset(manager, "Roboto-Regular.ttf");
 		paint = new Paint();
+		paint.setTypeface(typeface);
 		float size = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, SIZE_KEYBOARD, ContextHolder.getContext().getResources().getDisplayMetrics());
 		paint.setTextSize(size);
 	}

H3nb/JL-Mod-Plus

(cherry picked from commit 2d08da132e8056c0058353cdf851e7f998ee6e43)

app/src/main/java/javax/microedition/lcdui/Font.java
@@ -17,9 +17,12 @@
 
 package javax.microedition.lcdui;
 
+import android.content.res.AssetManager;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.util.TypedValue;
+
+import java.util.Arrays;
 
 import javax.microedition.util.ContextHolder;
 
@@ -47,9 +50,7 @@ public class Font {
 	public static void setApplyDimensions(boolean flag) {
 		applyDimensions = flag;
 
-		for (int i = 0; i < fonts.length; i++) {
-			fonts[i] = null;
-		}
+		Arrays.fill(fonts, null);
 	}
 
 	public static void setSize(int size, float value) {
@@ -70,9 +71,7 @@ public class Font {
 				return;
 		}
 
-		for (int i = 0; i < fonts.length; i++) {
-			fonts[i] = null;
-		}
+		Arrays.fill(fonts, null);
 	}
 
 	private Paint paint;
@@ -94,7 +93,10 @@ public class Font {
 
 	// Font for keyboard
 	public Font() {
+		AssetManager manager = ContextHolder.getAppContext().getAssets();
+		Typeface typeface = Typeface.createFromAsset(manager, "Roboto-Regular.ttf");
 		paint = new Paint();
+		paint.setTypeface(typeface);
 		float size = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, SIZE_KEYBOARD, ContextHolder.getAppContext().getResources().getDisplayMetrics());
 		paint.setTextSize(size);
 	}