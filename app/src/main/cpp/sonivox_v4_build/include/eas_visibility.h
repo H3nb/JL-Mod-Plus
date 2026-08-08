77c79d96040eb62381333caea4743d41ed447753
Hendra Bara'langi <93023716+H3nb@users.noreply.github.com>
2026-08-08 20:33:53 +0800

    build(audio): provide static SONiVOX visibility header

---
+++ app/src/main/cpp/sonivox_v4_build/include/eas_visibility.h
@@ -0,0 +1,8 @@
+#ifndef JL_MOD_SONIVOX_V4_EAS_VISIBILITY_H
+#define JL_MOD_SONIVOX_V4_EAS_VISIBILITY_H
+
+/* SONiVOX is linked statically into mmapi_eas. */
+#define EAS_PUBLIC
+#define EAS_PRIVATE
+
+#endif // JL_MOD_SONIVOX_V4_EAS_VISIBILITY_H
