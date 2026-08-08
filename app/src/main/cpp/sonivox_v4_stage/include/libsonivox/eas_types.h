a87cd681c4710fd9621a984433d28215a5fd3951
Hendra Bara'langi <93023716+H3nb@users.noreply.github.com>
2026-08-08 20:48:58 +0800

    build(audio): add SONiVOX 4 type include wrapper [skip ci]

---
+++ app/src/main/cpp/sonivox_v4_stage/include/libsonivox/eas_types.h
@@ -0,0 +1,6 @@
+#ifndef JL_MOD_PLUS_SONIVOX_V4_PUBLIC_EAS_TYPES_H
+#define JL_MOD_PLUS_SONIVOX_V4_PUBLIC_EAS_TYPES_H
+
+#include "../../../sonivox_v4/arm-wt-22k/host_src/eas_types.h"
+
+#endif // JL_MOD_PLUS_SONIVOX_V4_PUBLIC_EAS_TYPES_H
