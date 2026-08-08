f96a62d95840ed653dab840b0b3d29629e895acb
Hendra Bara'langi <93023716+H3nb@users.noreply.github.com>
2026-08-08 11:40:14 +0800

    test implicit video dimension compatibility

--- app/src/test/java/javax/microedition/media/camera/CaptureLocatorParserTest.java
+++ app/src/test/java/javax/microedition/media/camera/CaptureLocatorParserTest.java
@@ -65,6 +65,17 @@ public class CaptureLocatorParserTest {
 	}
 
 	@Test
+	public void implicitVideoEncodingKeepsLegacySourceDimensions() throws Exception {
+		CaptureRequest request = CaptureLocatorParser.parse(
+				"capture://video?width=640&height=480");
+
+		assertEquals(CaptureRequest.VIDEO_RECORDING_CONTENT_TYPE, request.getEncoding());
+		assertEquals(640, request.getWidth());
+		assertEquals(480, request.getHeight());
+		assertTrue(request.hasExplicitDimensions());
+	}
+
+	@Test
 	public void acceptsAdvertisedMp4EncodingForVideoRecording() throws Exception {
 		CaptureRequest request = CaptureLocatorParser.parse("capture://video?encoding=video%2Fmp4");
 
@@ -76,8 +87,6 @@ public class CaptureLocatorParserTest {
 	public void rejectsExplicitMp4RecordingDimensionsUntilTheyCanBeExact() {
 		assertThrows(MediaException.class, () -> CaptureLocatorParser.parse(
 				"capture://video?encoding=video%2Fmp4&width=640&height=480"));
-		assertThrows(MediaException.class, () -> CaptureLocatorParser.parse(
-				"capture://video?width=640&height=480"));
 		assertThrows(MediaException.class, () -> CaptureLocatorParser.parse(
 				"capture://audio_video?width=640&height=480"));
 	}
