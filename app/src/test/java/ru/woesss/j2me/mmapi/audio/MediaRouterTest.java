67ecad65a4b9f0fda47efabf9cb97ef70316567e
Hendra Bara'langi <93023716+H3nb@users.noreply.github.com>
2026-08-08 19:36:23 +0800

    test(audio): cover deterministic backend routing

---
+++ app/src/test/java/ru/woesss/j2me/mmapi/audio/MediaRouterTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2026 H3NB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ru.woesss.j2me.mmapi.audio;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MediaRouterTest {
+	@Test
+	public void contentSignatureWinsOverConflictingMime() {
+		assertEquals(MediaRouter.Backend.PLATFORM_AUDIO,
+				MediaRouter.route(ContentProbe.Kind.WAV, "audio/midi"));
+		assertEquals(MediaRouter.Backend.SYNTH,
+				MediaRouter.route(ContentProbe.Kind.MIDI, "audio/wav"));
+	}
+
+	@Test
+	public void sequencedMediaUsesSynthBackend() {
+		assertEquals(MediaRouter.Backend.SYNTH,
+				MediaRouter.route(ContentProbe.Kind.MIDI, null));
+		assertEquals(MediaRouter.Backend.SYNTH,
+				MediaRouter.route(ContentProbe.Kind.XMF, null));
+		assertEquals(MediaRouter.Backend.SYNTH,
+				MediaRouter.route(ContentProbe.Kind.RMID, null));
+	}
+
+	@Test
+	public void decodedAndPlatformFormatsBypassSynth() {
+		assertEquals(MediaRouter.Backend.PLATFORM_AUDIO,
+				MediaRouter.route(ContentProbe.Kind.WAV, null));
+		assertEquals(MediaRouter.Backend.PLATFORM_AUDIO,
+				MediaRouter.route(ContentProbe.Kind.MP3, null));
+		assertEquals(MediaRouter.Backend.PLATFORM_AUDIO,
+				MediaRouter.route(ContentProbe.Kind.AAC, null));
+		assertEquals(MediaRouter.Backend.PLATFORM_AUDIO,
+				MediaRouter.route(ContentProbe.Kind.AMR, null));
+		assertEquals(MediaRouter.Backend.PLATFORM_AUDIO,
+				MediaRouter.route(ContentProbe.Kind.AMR_WB, null));
+		assertEquals(MediaRouter.Backend.PLATFORM_AUDIO,
+				MediaRouter.route(ContentProbe.Kind.MP4, null));
+	}
+
+	@Test
+	public void mimeOnlyFallbackIsConservative() {
+		assertEquals(MediaRouter.Backend.SYNTH,
+				MediaRouter.route(ContentProbe.Kind.UNKNOWN, " Audio/MIDI; charset=binary "));
+		assertEquals(MediaRouter.Backend.PLATFORM_AUDIO,
+				MediaRouter.route(ContentProbe.Kind.UNKNOWN, "audio/x-wav"));
+		assertEquals(MediaRouter.Backend.UNKNOWN,
+				MediaRouter.route(ContentProbe.Kind.UNKNOWN, "audio/mmf"));
+		assertEquals(MediaRouter.Backend.UNKNOWN,
+				MediaRouter.route(ContentProbe.Kind.UNKNOWN, "audio/ogg"));
+		assertEquals(MediaRouter.Backend.UNKNOWN,
+				MediaRouter.route(ContentProbe.Kind.UNKNOWN, null));
+	}
+}
