a46acbc2b00563c5ac900c081b40cafc800ed3a7
Hendra <hendrabaralangi13@gmail.com>
2026-08-23 18:23:34 +0800

    Gate timing sessions on transformed artifacts

---
+++ app/src/main/java/javax/microedition/shell/timing/TimingTransformMetadata.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package javax.microedition.shell.timing;
+
+import java.util.Map;
+
+import javax.microedition.shell.GuestTimingBridge;
+
+/** Compatibility marker stored in the converted descriptor next to the DEX artifact. */
+public final class TimingTransformMetadata {
+	public static final int TRANSFORM_VERSION = 1;
+	public static final String TRANSFORM_VERSION_ATTRIBUTE = "JLMod-Timing-Transform-Version";
+	public static final String BRIDGE_ABI_ATTRIBUTE = "JLMod-Timing-Bridge-ABI";
+
+	private TimingTransformMetadata() {
+	}
+
+	public static void mark(Map<String, String> attributes) {
+		if (attributes == null) {
+			throw new NullPointerException("attributes");
+		}
+		attributes.put(TRANSFORM_VERSION_ATTRIBUTE, Integer.toString(TRANSFORM_VERSION));
+		attributes.put(BRIDGE_ABI_ATTRIBUTE, Integer.toString(GuestTimingBridge.ABI_VERSION));
+	}
+
+	public static boolean isCompatible(Map<String, String> attributes) {
+		if (attributes == null) {
+			return false;
+		}
+		return Integer.toString(TRANSFORM_VERSION).equals(
+				attributes.get(TRANSFORM_VERSION_ATTRIBUTE))
+				&& Integer.toString(GuestTimingBridge.ABI_VERSION).equals(
+						attributes.get(BRIDGE_ABI_ATTRIBUTE));
+	}
+}
