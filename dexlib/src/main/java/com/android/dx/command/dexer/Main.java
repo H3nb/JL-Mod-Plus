f7a5e805812f2e73acf11047fac7e55cfa5027ea
Hendra <hendrabaralangi13@gmail.com>
2026-08-25 21:50:27 +0800

    docs: annotate dex metadata integration

--- dexlib/src/main/java/com/android/dx/command/dexer/Main.java
+++ dexlib/src/main/java/com/android/dx/command/dexer/Main.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2007 The Android Open Source Project
+ * Modified in 2026 for Memory Editor metadata sidecar output.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
