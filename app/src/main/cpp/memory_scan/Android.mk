251bc43e5318edad241b5dacd9c998f1e41d7896
Hendra Bara'langi <93023716+H3nb@users.noreply.github.com>
2026-08-27 11:49:38 +0800

    Make libjlmem the memory editor backbone

--- app/src/main/cpp/memory_scan/Android.mk
+++ app/src/main/cpp/memory_scan/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := memory_scan
-LOCAL_SRC_FILES := memory_scan.cpp memory_scan_jni_aliases.cpp
+LOCAL_MODULE := jlmem
+LOCAL_SRC_FILES := memory_scan.cpp memory_scan_jni_aliases.cpp live_tracker.cpp
 LOCAL_CPP_FEATURES := exceptions
 LOCAL_CPPFLAGS += -std=c++17 -Wall -Wextra -include cstdio -include cstdlib -include new
 include $(BUILD_SHARED_LIBRARY)
