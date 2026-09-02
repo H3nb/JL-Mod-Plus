1263026066c6bb245f1bebb8841972ebcd536c61
Hendra Bara'langi <93023716+H3nb@users.noreply.github.com>
2026-09-02 19:19:15 +0800

    [skip ci] Compile ResultStore lazy cursor

--- app/src/main/cpp/memory/Android.mk
+++ app/src/main/cpp/memory/Android.mk
@@ -10,7 +10,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := jlmem
-LOCAL_SRC_FILES := memory_engine.cpp mutation_bridge.cpp result_store.cpp result_store_scan.cpp result_store_shadow_bridge.cpp
+LOCAL_SRC_FILES := memory_engine.cpp mutation_bridge.cpp result_store.cpp result_store_scan.cpp result_store_shadow_bridge.cpp result_cursor.cpp
 LOCAL_CPPFLAGS := -std=c++23 -Wall -Wextra -Werror -Wpedantic -Wformat=2 -Wimplicit-fallthrough
 LOCAL_CPP_FEATURES := exceptions
 LOCAL_LDLIBS := -llog
