bcbc2423c6b292933d73b6229d421f74815efbd6
Hendra Bara'langi <93023716+H3nb@users.noreply.github.com>
2026-09-05 13:01:02 +0800

    memory: include V2-aware filter compatibility seam

--- app/src/main/cpp/memory/memory_engine_production.cpp
+++ app/src/main/cpp/memory/memory_engine_production.cpp
@@ -16,3 +16,4 @@
 #include "memory_engine_relative_extension.inc"
 #include "memory_engine_refresh_extension.inc"
 #include "memory_engine_revision_extension.inc"
+#include "memory_engine_filter_compat_extension.inc"
