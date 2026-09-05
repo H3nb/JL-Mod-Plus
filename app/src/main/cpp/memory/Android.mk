712eb8ab355e259d43d507e49ecdcc7276a5ebba
Hendra Bara'langi <93023716+H3nb@users.noreply.github.com>
2026-09-05 17:29:33 +0800

    Build Group Search target fixture

--- app/src/main/cpp/memory/Android.mk
+++ app/src/main/cpp/memory/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := jlmem_target
-LOCAL_SRC_FILES := target_probe.cpp
+LOCAL_SRC_FILES := target_probe.cpp group_probe.cpp
 LOCAL_CPPFLAGS := -std=c++23 -Wall -Wextra -Werror -Wpedantic -Wformat=2 -Wimplicit-fallthrough
 LOCAL_CPP_FEATURES := exceptions
 LOCAL_LDLIBS := -llog
