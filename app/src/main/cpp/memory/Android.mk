ecd2a7f712e17aa0da33e3bf0505ac0ac95ddd5f
Hendra Bara'langi <93023716+H3nb@users.noreply.github.com>
2026-09-05 13:10:28 +0800

    memory: build typed alias cursor and compact sidecar

--- app/src/main/cpp/memory/Android.mk
+++ app/src/main/cpp/memory/Android.mk
@@ -11,9 +11,11 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE := jlmem
 JL_MEM_PRODUCTION_SRC := memory_engine_production.cpp mutation_bridge.cpp result_store.cpp \
-    result_store_scan.cpp result_store_refine.cpp result_store_relative.cpp result_cursor.cpp
+    result_store_scan.cpp result_store_refine.cpp result_store_relative.cpp result_cursor.cpp \
+    result_alias_cursor.cpp ordinary_result_store.cpp
 ifeq ($(NDK_DEBUG),1)
-LOCAL_SRC_FILES := $(JL_MEM_PRODUCTION_SRC) result_store_shadow_bridge.cpp result_store_auto_probe.cpp
+LOCAL_SRC_FILES := $(JL_MEM_PRODUCTION_SRC) result_store_shadow_bridge.cpp result_store_auto_probe.cpp \
+    ordinary_result_store_probe.cpp
 else
 LOCAL_SRC_FILES := $(JL_MEM_PRODUCTION_SRC) memory_engine_compat_bridge.cpp
 endif
