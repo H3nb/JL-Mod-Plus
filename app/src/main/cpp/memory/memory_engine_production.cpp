92976d39384eeba403772cb7724319d25a599787
Hendra Bara'langi <93023716+H3nb@users.noreply.github.com>
2026-09-05 16:43:35 +0800

    Compile compact owner cutover layer

--- app/src/main/cpp/memory/memory_engine_production.cpp
+++ app/src/main/cpp/memory/memory_engine_production.cpp
@@ -18,6 +18,7 @@
 #include "memory_engine_revision_extension.inc"
 #include "memory_engine_ordinary_extension.inc"
 #include "memory_engine_compact_extension.inc"
+#include "memory_engine_compact_owner_extension.inc"
 #include "memory_engine_ordinary_debug_bridge.inc"
 #include "memory_engine_filter_compat_extension.inc"
 #include "memory_engine_search_compat_extension.inc"
